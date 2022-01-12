68f21b7d86a5a72464800b168488dddaccd0aa63
soha <soha@cs-soha-u.cs.umn.edu>
2022-01-12 09:46:55 -0600

    adding logs for partial problem as well as the script combine_coverage_per_thread

--- pyScripts/combine_coverage_per_thread.py
+++ pyScripts/combine_coverage_per_thread.py
@@ -1,57 +1,78 @@
 #!/usr/bin/env python3
-import numpy as np
 import os
 import pandas as pd
 
 execution_log_dir = os.getcwd() + '/logs/coverage_log/'
-content_dfs = []
+# content_dfs = []
 df_columns = []
+i = 0
 for benchmark_log_dir in os.listdir(execution_log_dir):
     for benchmark_log_file in os.listdir(os.path.join(execution_log_dir, benchmark_log_dir)):
-        with open(os.path.join(execution_log_dir, benchmark_log_dir, benchmark_log_file),
-                  'r') as f:
+        with open(os.path.join(execution_log_dir, benchmark_log_dir, benchmark_log_file), 'r') as f:
             content_df = pd.read_csv(f)
             file_name = f.name
             if 'ThreadStat_' in file_name:
-                content_dfs.append(content_df[content_df.columns[1]][1:])
+                # content_dfs.append(content_df[content_df.columns[1]])
                 mode = ''
                 if '0' in file_name:
                     mode = 'SPF'
+                    spf_df = content_df[content_df.columns[1]].values
+                    # print('spf found, printing column[1]')
+                    # print(content_df[content_df.columns[1]])
+                    # print('end')
                 elif '2' in file_name:
                     mode = 'mode2'
+                    jr_mode2 = content_df[content_df.columns[1]].values
+                    # print('mode2 found, printing column[1]')
+                    # print(content_df[content_df.columns[1]])
+                    # print('end')
                 elif '3' in file_name:
                     mode = 'mode3'
+                    jr_mode3 = content_df[content_df.columns[1]].values
+                    # print('mode3 found, printing column[1]')
+                    # print(content_df[content_df.columns[1]])
+                    # print('end')
                 elif '4' in file_name:
                     mode = 'mode4'
+                    jr_mode4 = content_df[content_df.columns[1]].values
+                    # print('mode4 found, printing column[1]')
+                    # print(content_df[content_df.columns[1]])
+                    # print('end')
                 elif '5' in file_name:
                     mode = 'mode5'
+                    jr_mode5 = content_df[content_df.columns[1]].values
+                    # print('mode5 found, printing column[1]')
+                    # print(content_df[content_df.columns[1]])
+                    # print('end')
                 else:
                     print('error wrong files.aborting')
                     exit(1)
                 df_columns.append(benchmark_log_dir+'_' + mode)
+                i += 1
 
-    spf_df = ''
-    jr_mode2 = ''
-    jr_mode3 = ''
-    jr_mode4 = ''
-    jr_mode5 = ''
-
-    for i in range(len(df_columns)):
-        if 'SPF' in df_columns[i]:
-            spf_df = content_dfs[i]
-        elif 'mode2' in df_columns[i]:
-            jr_mode2 = content_dfs[i]
-        elif 'mode3' in df_columns[i]:
-            jr_mode3 = content_dfs[i]
-        elif 'mode4' in df_columns[i]:
-            jr_mode4 = content_dfs[i]
-        elif 'mode5' in df_columns[i]:
-            jr_mode5 = content_dfs[i]
-        else:
-            print('error wrong files.aborting')
-            exit(1)
-
-    new_data_frame = pd.concat([spf_df, jr_mode2, jr_mode3, jr_mode4, jr_mode5])
+    # new_data_frame = pd.DataFrame([spf_df, jr_mode2, jr_mode3, jr_mode4, jr_mode5])
+    df_columns.sort()
+    # print(df_columns)
     print(','.join(df_columns))
 
-    new_data_frame.to_csv(benchmark_log_dir+'_combined_coverage_per_thread.cvs', axis = 1)
+    output_file = str(benchmark_log_dir) + '_combined_coverage_per_thread.cvs'
+
+    if(spf_df.size == 0 or jr_mode2.size == 0 or jr_mode3.size == 0 or jr_mode4.size == 0 or jr_mode5.size == 0):
+        print('one run is missing. failing.')
+        exit(1)
+
+    with open(output_file, 'w') as output_file_pointer:
+        output_file_pointer.write(','.join(df_columns))
+        for i in range(max(spf_df.size, jr_mode2.size, jr_mode3.size, jr_mode4.size, jr_mode5.size)):
+            spf_str = str(spf_df[i]) if i < spf_df.size else '_'
+            jr_mode2_str = str(jr_mode2[i]) if i < jr_mode2.size else '_'
+            jr_mode3_str = str(jr_mode3[i]) if i < jr_mode3.size else '_'
+            jr_mode4_str = str(jr_mode4[i]) if i < jr_mode4.size else '_'
+            jr_mode5_str = str(jr_mode5[i]) if i < jr_mode5.size else '_'
+
+            line = '\n' + spf_str + "," + jr_mode2_str + ',' + jr_mode3_str + ',' + jr_mode4_str+',' + jr_mode5_str
+            output_file_pointer.write(line)
+
+
+
+    # new_data_frame.to_csv(output_file, mode='a', index=False, header=False)
